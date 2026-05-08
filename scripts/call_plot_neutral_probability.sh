ec5bb1b103c47154b58440160256d1c855441241
noemiandor <noemi.andor@gmail.com>
2026-05-08 11:30:47 -0400

    heatmap call added to caller

--- scripts/call_plot_neutral_probability.sh
+++ scripts/call_plot_neutral_probability.sh
@@ -7,3 +7,7 @@ rm result_summaries/forward_validation_*_summary.Rds results/forward_validation_
 #Rscript scripts/run_forward_validation_all.R     --grouped_intervals_path=core_data/grouped_intervals.Rds     --group_fit_path=results/group_fit_df.Rds     --output_dir=results     --summary_output_dir=result_summaries    --n_reps=30 --n_null_pairs=100  --n_cores=4  --bottleneck_size=1000  --expansion_factor=32 --distance_metric=chrom_weighted_wasserstein
 
 Rscript scripts/run_forward_validation_all.R --grouped_intervals_path=core_data/grouped_intervals.Rds --group_fit_path=results/group_fit_df.Rds --output_dir=results --summary_output_dir=result_summaries --n_reps=200 --n_null_pairs=1000 --n_cores=7 --bottleneck_size=3000 --expansion_factor=32 --distance_metric=wasserstein
+
+Rscript scripts/plot_neutral_probability_neglog10p.R --heatmap_distance=chrom_weighted     --heatmap_cluster_mode=auto --heatmap_cluster_score=calinski_harabasz
+
+
